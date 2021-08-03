55c25b80fbcbbdb76640fcf376c4cfec8f259a31
Leonardo Rincón <jleobotache@gmail.com>
2021-08-02 22:22:27 -0500

    Add styles and validation form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './Form.scss';
 
 export const Form = ({ name }) => {
+  const [error, setError] = useState(false);
+  const [lightbox, setLightbox] = useState(false);
   const [state, setstate] = useState({
     nombre: '',
     email: '',
@@ -15,31 +17,72 @@ export const Form = ({ name }) => {
       [e.target.name]: e.target.value,
     });
   };
-  console.log(state);
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (
+      state.nombre === '' ||
+      state.email === '' ||
+      state.celular === '' ||
+      state.edad === ''
+    ) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 2000);
+      return;
+    }
 
+    setLightbox(true);
+
+    setstate({
+      nombre: '',
+      email: '',
+      celular: '',
+      edad: '',
+    });
+  };
   return (
     <main>
       <form className='form'>
-        <p className='form--title'>
+        {error && (
+          <p className='form--message-error'>
+            **debe diligenciar todos los campos**
+          </p>
+        )}
+        <h3 className='form--title'>
           Hola, bienvenido, sabemos que quieres viajar en un {name}
-        </p>
-        <label>Nombre Completo</label>
+        </h3>
+        <label>Nombre Completo*</label>
         <input
+          value={state.nombre}
           type='text'
           name='nombre'
           onChange={actualizarState}
           placeholder='Nombre Completo'
         />
-        <label>Email</label>
-        <input type='email' />
-        <label>Celular</label>
-        <input type='number' />
-        <label>Edad</label>
-        <select type='text'>
-          <option value='1'>18 a 40</option>
-          <option value='2'>41 a 60</option>
-          <option value='3'>61 a 100</option>
+        <label>Email*</label>
+        <input
+          value={state.email}
+          type='email'
+          name='email'
+          onChange={actualizarState}
+          placeholder='example@example.com'
+        />
+        <label>Celular*</label>
+        <input
+          value={state.celular}
+          type='number'
+          name='celular'
+          onChange={actualizarState}
+          placeholder='+57 43434343'
+        />
+        <label>Edad*</label>
+        <select type='text' name='edad' onChange={actualizarState}>
+          <option value='18 a 40'>18 a 40</option>
+          <option value='41 a 60'>41 a 60</option>
+          <option value='61 a 100'>61 a 100</option>
         </select>
+        <button onClick={handleClick}>Agregar</button>
       </form>
     </main>
   );
